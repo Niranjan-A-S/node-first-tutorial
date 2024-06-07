ead56129f40e4bfa4e30a985fd97421de8bd5196
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-06-07 17:05:52 +0530

    character sets and encodings

---
+++ character-sets-encoding/index.js
@@ -0,0 +1,14 @@
+console.log("V".charCodeAt()); // gives the numeric definition of a character 
+
+//!Character Sets
+//Character Sets are predefined list of characters represented by numbers
+//Popular Character Sets are Unicode and ASCII
+//Unicode determines V should be represented as 86
+
+//!Character Encoding
+//Character Encoding dictates how to represent a number in a character set as binary data before it can be stored inside the computer
+//It dictates how many bits are used to represent the number
+//Example of a character encoding is UTF-8
+//UTF-8 states that the character should be encoded in bytes (8 bits)
+//Eight 1s or 0s should be used to represent code of any character in binary
+//4 => 100 => 00000100
