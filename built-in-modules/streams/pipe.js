94ca3342369a5ef1c507e8150930e3ffd823168e
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-06-10 13:19:23 +0530

    pipe

---
+++ built-in-modules/streams/pipe.js
@@ -0,0 +1,12 @@
+// const fs = require('node:fs');
+
+// const readableStream = fs.createReadStream('./1.txt', {
+//     encoding: "utf-8",
+//     highWaterMark: 2
+// });
+
+// const writeableStream = fs.createWriteStream('./2.txt');
+
+// readableStream.pipe(writeableStream);
+//the pipe method  returns the writeable stream
+
