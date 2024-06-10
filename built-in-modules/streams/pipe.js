64e766179ee54cf20e4fe219e17ba38d9c722069
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-06-10 13:21:20 +0530

    transform

--- built-in-modules/streams/pipe.js
+++ built-in-modules/streams/pipe.js
@@ -1,4 +1,4 @@
-// const fs = require('node:fs');
+const fs = require('node:fs');
 
 // const readableStream = fs.createReadStream('./1.txt', {
 //     encoding: "utf-8",
@@ -10,3 +10,13 @@
 // readableStream.pipe(writeableStream);
 //the pipe method  returns the writeable stream
 
+const zlib = require('node:zlib');
+
+const gzip = zlib.createGzip();
+
+const readableStream = fs.createReadStream('./1.txt', {
+    encoding: "utf-8",
+    highWaterMark: 2
+});
+
+readableStream.pipe(gzip).pipe(fs.WriteStream('./2.txt.gz'));
